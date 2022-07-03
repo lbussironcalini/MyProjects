f4aee808eb04cdf2b8943da81151e88d78b57736
lbussironcalini <l.bussironcalini@gmail.com>
2022-07-03 20:47:41 +0200

    Update App.java
    test 5

--- Test/src/main/java/com/example/Test/App.java
+++ Test/src/main/java/com/example/Test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World I'm Lucia" );
+        System.out.println( "Hello World I'm Lucia Bussi" );
     }
 }
